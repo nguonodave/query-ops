93f09aad286785669d9aa8888655ba52d2fb1cf2
David Ochiel <nguonodave@gmail.com>
2025-06-06 09:26:25 +0300

    feat: add auditRatio in query

--- static/js/app/graphQl.js
+++ static/js/app/graphQl.js
@@ -7,6 +7,7 @@ export async function fetchUserProfile(token) {
         # Basic Identification
         login
         attrs
+        auditRatio
         
         # XP Data (for line graph)
         xpTransactions: transactions(
